bbfa7a517b130d2153a577a4c1c0b65e6156dc23
aravindcad01 <aravind2k23@gmail.com>
2023-12-30 16:06:31 +0530

    updated all files

---
+++ script.js
@@ -0,0 +1,77 @@
+function add()
+{
+    var a=10;
+    var b=20;
+    var c=a+b;
+    console.log("value of a is..."+a);
+    console.log("value of b is..."+b);
+    console.log("add the value ..."+c);
+}
+add();
+
+function Sub()
+{
+    var a=20;
+    var b=30;
+    var c=a-b;
+    console.log("value of a is..."+a);
+    console.log("value of b is..."+b);
+    console.log("subtracted the value...."+c);
+   
+}
+Sub();
+  
+
+function mul()
+{
+    var a=50;
+    var b=30;
+    var c=a*b;
+    
+     console.log("value of a is..."+a);
+    console.log("value of b is..."+b);
+    console.log("mul the value...."+c);
+    
+}
+mul();
+var a=40;
+var b=20;
+console.log("div the value..."+(a/b));
+console.log(a**b);
+console.log(a+-b);
+console.log(a+=b);
+console.log(a-=b);
+console.log(a*=b);
+console.log(a%=b);
+console.log(a**=b);
+console.log(a>b);
+console.log(a<b);
+console.log(a!=b);
+console.log(a!==b);
+console.log(a<=b);
+console.log(a>=b);
+console.log(a&&b);
+
+
+document.getElementById("fnamed").innerHTML=document.getElementById("fname").value;
+document.getElementById("lnamed").innerHTML=document.getElementById("lname").value;
+document.getElementById("pno").innerHTML=document.getElementById("pn").value;
+
+function assign();
+var a=30;
+var b=50;
+c=a+b;
+console.log("value of a is ..."+a);
+console.log("value of b")
+
+
+
+
+
+
+
+
+
+    }
+}
+  
