85e5c06a7bb99af0e6f72a93765814284bf8aa69
aravindcad01 <aravind2k23@gmail.com>
2024-01-20 18:08:33 +0530

    added

--- js/loop.js
+++ js/loop.js
@@ -2,4 +2,14 @@ function onlinereg(){
     var application=document.getElementById("apply").value;
     console.log(application);
     console.log(typeof application );
+    application=Number(application);
+    console.log(application);
+    console.log(typeof application );
+
+
+while(application<20){
+    console.log("application "+application+" smaller than 20");
+    application++;
+
 }
+}onlinereg();
